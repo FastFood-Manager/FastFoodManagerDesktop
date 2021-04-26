b7c0961647c5285af97dc961f29412cf56276432
Henrique Oliveira <64371355+Henrique-Oli-Mattos@users.noreply.github.com>
2021-04-26 20:32:43 -0300

    Add files via upload
    Splash Screen
    FastFood_Manager será a classe main, com objetivo de instanciar e chamar a Splash Screen e após isso chamar menu

---
+++ FastFoodManager/src/UI/FastFood_Manager.java
@@ -0,0 +1,14 @@
+package fastfood_manager;
+
+/**
+ *
+ * @author Henrique
+ */
+public class FastFood_Manager {
+
+    public static void main(String[] args) {
+        SplashScreen Splash = new SplashScreen();
+        Splash.setVisible(true);     
+    }
+    
+}
